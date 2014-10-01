bb6241db250533e8e02b804faa5dfb3657b521e7
Rania M. Masri <RaniaMasri@Ranias-MacBook-Pro-2.local>
2014-10-01 01:56:56 -0400

    ARTG5330 Visualization TechnologiesAssignment 1

---
+++ script/script.js
@@ -0,0 +1,42 @@
+$(".controls .button").on("click",function(d){
+    d.preventDefault();
+
+    var idValue = $(d.target).attr('id');
+
+    $('.dev-log')
+        .append("<p>Clicked " + idValue + "</p>");
+});
+
+$(".button,:radio,:checkbox").on("change",function(e){
+    var idValue = $(e.target).attr('id');
+
+    $('.dev-log')
+        .append("<p>Clicked " + idValue + "</p>");
+});
+
+$("#selectinput").on("change",function(e){
+    var value = $(e.target).val();
+
+    $('.dev-log')
+        .append("<p>Clicked selectinput &gt; " + value + "</p>");
+});
+
+$("#textinput").on("input",function(e){
+    var value = $(e.target).val();
+
+    $('.dev-log')
+        .append("<p>Typed in textinput: " + value + "</p>");
+});
+
+
+//    $("div").click
+
+//    $("button").on("click", function(){
+//        .html("dev-log", )
+//    })
+//    $("radio").on("change", )
+//    $("checkbox").on("change", )
+//    $("select").on("change", )
+//    $("text").on("change", )
+
+
